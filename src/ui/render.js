a27255eec9701b379871ded8accd094696761a0d
Hamadellah <hamadellahotman13@gmail.com>
2026-02-27 12:28:45 +0000

    Refactor recipe rendering and styling; enhance detail view and add new grid layout

--- src/ui/render.js
+++ src/ui/render.js
@@ -1,4 +1,4 @@
-// src/ui/render.js
+
 import { getBadgeColor } from "../services/calorieService.js";
 
 export function renderRecipes(recipes) {
@@ -6,9 +6,9 @@ export function renderRecipes(recipes) {
   grid.innerHTML = "";
 
   recipes.forEach((recipe) => {
-    const badgeClass = getBadgeColor(recipe.caloriesPerServing); //
+    const badgeClass = getBadgeColor(recipe.caloriesPerServing); 
 
-    // زِدنا class "btn-see" و data-id باش نعرفو آينا وصفة تكليركات
+  
     const cardHTML = `
             <article class="recipe-card" data-id="${recipe.id}">
                 <div class="card-image-container">
@@ -17,9 +17,6 @@ export function renderRecipes(recipes) {
                 </div>
                 <div class="card-content">
                     <h3>${recipe.name}</h3>
-                    <div class="card-footer">
-                        <span class="rating">⭐ ${recipe.rating}</span>
-                    </div>
                     <button class="btn-see" data-id="${recipe.id}">See More</button>
                 </div>
             </article>
@@ -30,9 +27,10 @@ export function renderRecipes(recipes) {
 
 export function renderRecipeDetail(recipe) {
   const grid = document.getElementById("recipe-grid");
-  grid.innerHTML = ""; // كنخويو الـ grid باش نبينو غير هاد الوصفة
+  grid.innerHTML = ""; 
 
-  const detailHTML = `
+    const detailHTML = `
+  <div class="hh">
         <div class="recipe-detail-view">
             <button id="btn-back" class="btn-back">⬅️ Back</button>
             <div class="detail-header">
@@ -44,7 +42,7 @@ export function renderRecipeDetail(recipe) {
                 <div class="section">
                     <h3>Ingredients (${recipe.ingredients.length})</h3>
                     <ul>
-                        ${recipe.ingredients.map((ing) => `<li>✅ ${ing}</li>`).join("")}
+                        ${recipe.ingredients.map((ing) => `<li>- ${ing}</li>`).join("")}
                     </ul>
                 </div>
 
@@ -55,6 +53,7 @@ export function renderRecipeDetail(recipe) {
                     </ol>
                 </div>
             </div>
+        </div>
         </div>
     `;
   grid.innerHTML = detailHTML;
